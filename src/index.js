f2d39e0f625a34596d091a04c3ec484391a7acc7
shaher2017 <shaherhero8@gmail.com>
2023-11-25 09:59:11 +0200

    deploy

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {disableReactDevTools} from "@fvilers/disable-react-devtools";
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
